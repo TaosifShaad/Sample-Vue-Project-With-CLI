e2420f9fc13534aff5295ee5ed6ee8855d33f66c
Taosif <taosif@softzino.com>
2022-12-07 17:01:39 +0600

    vue with cli

---
+++ src/components/TravelCard.vue
@@ -0,0 +1,20 @@
+<template id="placeCard">
+ <div class="place">
+    <img :src="card.img" width="235" height="300"/>
+    <h2>{{ card.name }}</h2>
+    <p>{{ card.desc }}</p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'TravelCard',
+  props: {
+    card: String
+  }
+}
+</script>
+
+<style>
+
+</style>
